732af9ca2cdfe174310452f6937647851e622837
Debargha Nandi <debarghanandi.india@gmail.com>
2023-09-16 23:49:14 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Mainbody from "./components/Mainbody";
+import Navbar from "./components/Navbar";
+import Menu from "./components/Menu";
+import About from "./components/About";
+import Location from "./components/Location";
+import Contact from "./components/Contact";
+
+function App() {
+  return (
+    <>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route element={<Mainbody/>} path="/"/>
+      <Route element={<Menu/>} path="/menu"/>
+      <Route element={<About/>} path="/about"/>
+      <Route element={<Location/>} path="/location"/>
+      <Route element={<Contact/>} path="/contact"/>
+    </Routes>
+    </BrowserRouter>
+    </>
+    // <>
+    // <Navbar/>
+    // <Mainbody/>
+    // </>
+  )
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav>
+        <div className="logo">
+            <NavLink to="/"><img src="/images/brand_logo.png" alt="" /></NavLink>
+        </div>
+        <ul>
+            <li>
+                <NavLink to="/menu" className="listStyle">MENU</NavLink>
+            </li>
+            <li>
+                <NavLink to="/location" className="listStyle">LOCATION</NavLink>
+            </li>
+            <li>
+                <NavLink to="/about" className="listStyle">ABOUT</NavLink>
+            </li>
+            <li>
+                <NavLink to="/contact" className="listStyle">CONTACT</NavLink>
+            </li>
+        </ul>
+        <button>Login</button>
+      </nav>
+    </div>
+  )
+}
